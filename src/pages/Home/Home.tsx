668d310f848e61dfa8e36e3fe5af3fab22233945
runL1keHell <oleg204grishan@gmail.com>
2023-09-26 17:33:52 +0300

    feature: profilesettings page and home processing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import React from "react";
 
 // Sample image data
 const images = [
@@ -38,14 +39,22 @@ export const Home: React.FC = () => {
   };
 
   return (
-    <div className='w-[97%]'>
+    <div className='w-full'>
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index} className='w-[402px] h-[255px] ml-[42px]'>
-            <img src={image} className='w-full h-full' />
+            <img src={image} className='w-full h-full' alt="Playlist Image" />
           </div>
         ))}
       </Slider>
+
+      <div className="w-[100%] h-[70px] mt-[20px] flex justify-around text-white shadow-md shadow-inset">
+        <button className="w-1/5 border-r-[1px] border-[#00000033]">All</button>
+        <button className="w-1/5 border-r-[1px] border-[#00000033]">Genres</button>
+        <button className="w-1/5 border-r-[1px] border-[#00000033]">Most Played</button>
+        <button className="w-1/5 border-r-[1px] border-[#00000033]">New Releases</button>
+        <button className="w-1/5 border-r-[1px] border-[#00000033]">Featured</button>
+      </div>
     </div>
   );
 };
