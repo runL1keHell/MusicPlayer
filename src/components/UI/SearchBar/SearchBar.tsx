32e2aa9126220768e38acd74bd99e2d220ada396
runL1keHell <oleg204grishan@gmail.com>
2023-09-28 23:19:43 +0300

    feature: search functionality processing

--- src/components/UI/SearchBar/SearchBar.tsx
+++ src/components/UI/SearchBar/SearchBar.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import {NavigateFunction, useNavigate} from "react-router";
+import {useParams} from "react-router-dom";
 
 type SearchBarProps = {
     className?: string;
@@ -8,24 +10,32 @@ type SearchBarProps = {
 export const SearchBar: React.FC<SearchBarProps> = ({className, innerIcon}) => {
     const [showIcon, setShowIcon] = useState<boolean>(true);
     const [inputValue, setInputValue] = useState<string>("");
-    
+    const navigate: NavigateFunction = useNavigate();
+    let {searchText} = useParams();
     const handleInputFocus = () => {
         setShowIcon(false);
     };
     const handleInputBlur = () => {
         setShowIcon(true)
     };
-    
+    console.log(searchText)
     return (
         <div className={`w-[60%] h-[37px]  relative ${className}`}>
             <input 
                 className='w-[100%] h-[100%] pl-[10px] bg-white rounded-[19px] shadow-inner md:shadow-lg' 
                 type="text"
-                onChange={(e) => setInputValue(e.target.value)}
-                onFocus={handleInputFocus}
+                onChange={(e) => {
+                    setInputValue(e.target.value);
+                    searchText = e.target.value;
+                }}
+                onFocus={() => {
+                    handleInputFocus();
+                    navigate('/search');
+                }}
                 onBlur={() => {
                     inputValue ?  handleInputFocus() : handleInputBlur();
-                }} 
+                    inputValue ? null : navigate('/');
+                }}
                 onKeyDown={e => e.key === 'Enter' ? console.log('Enter has been pressed') : null}
             />
             {innerIcon && showIcon && 
--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -1,8 +1,6 @@
 import React from "react";
+import {useParams} from "react-router-dom";
 
 export const SearchResults:React.FC = () => {
-    return (
-        <>
-        </>
-    )
+    return
 }
